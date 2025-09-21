c4910941a1ed484298d87f60b9e63c0748e7dd17
lv-labs <luke@lv-labs.com>
2025-09-21 22:47:57 +0100

    fix url

--- Casks/afterdark.rb
+++ Casks/afterdark.rb
@@ -2,7 +2,7 @@ cask "afterdark" do
   version "1.0.0"
   sha256 "d93fcb68b342b2abb5557871f869eae3fec83f2debfd1a4a36cc8ade5951824c"
   
-  url "https://github.com/lv-labs/AfterDark/releases/download/v#{version}/AfterDark-#{version}.zip"
+  url "https://github.com/lv-labs/AfterDark/releases/download/v#{version}/AfterDark-v#{version}.zip"
   name "AfterDark"
   desc "Menu bar utility app"
   homepage "https://github.com/lv-labs/AfterDark"
